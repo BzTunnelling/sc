beb600072928b86156ae8f0a4fea81879de329f7
BzTunnelling <144299592+BzTunnelling@users.noreply.github.com>
2023-09-09 23:09:38 +0800

    Update edu.sh

--- websocket/edu.sh
+++ websocket/edu.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
-# youtube.com/@nyarigratisan
+# youtube.com/@anuy63
 # My Telegram : https://t.me/anuybazoelk
 # ==========================================
 # Color
-RED='\033[0;31m'
-NC='\033[0m'
+BLUE='\033[0;31m'
+RED='\033[0m'
 GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
+CYAN='\033[0;33m'
 BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
+RED='\033[0;35m'
+GREEN='\033[0;36m'
+CYAN='\033[0;37m'
 # ==========================================
 # Getting
 MYIP=$(wget -qO- icanhazip.com);
 IZIN=$( curl https://anuy639.github.io/izin | grep $MYIP )
 echo "Memeriksa Hak Akses VPS..."
 if [ $MYIP = $IZIN ]; then
-clear
-echo -e "${CYAN}Akses Diizinkan...${off}"
-sleep 1
+echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
-clear
-echo -e "${PURPLE}Akses Diblokir!${off}"
-echo "Hanya Untuk Pengguna Berbayar!"
-echo "Silahkan Hubungi Admin"
+echo -e "${NC}${RED}Permission Denied!${NC}";
+echo -e "${NC}${LIGHT}Please Contact Admin!!"
+echo -e "${NC}${LIGHT}Facebook : https://m.facebook.com/Anuybazoelk639"
+echo -e "${NC}${LIGHT}WhatsApp : https://wa.me/6285349326511"
+echo -e "${NC}${LIGHT}WhatsApp : https://wa.me/6287728411949"
+echo -e "${NC}${LIGHT}Youtube : youtube.com/@anuy63"
+echo -e "${NC}${LIGHT}Telegram : https://t.me/anuybazoelk"
+echo -e "${NC}${LIGHT}Telegram : https;//t.me/anuybazoelk639"
 exit 0
 fi
 clear
 echo "Checking VPS"
 clear
-# Link Hosting Kalian
-akbarvpn="raw.githubusercontent.com/anuy639/sc/main/websocket"
 
 # Getting Proxy Template
-wget -q -O /usr/local/bin/ws-nontls https://${akbarvpn}/websocket.py
+wget -q -O /usr/local/bin/ws-nontls https://raw.githubusercontent.com/anuy639/sc/main/websocket/websocket.py
 chmod +x /usr/local/bin/ws-nontls
 
 # Installing Service
@@ -62,7 +62,7 @@ systemctl enable ws-nontls
 systemctl restart ws-nontls
 
 # Getting Proxy Template
-wget -q -O /usr/local/bin/ws-ovpn https://${akbarvpn}/ws-ovpn.py
+wget -q -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/anuy639/sc/main/websocket/ws-ovpn.py
 chmod +x /usr/local/bin/ws-ovpn
 
 # Installing Service
@@ -90,7 +90,7 @@ systemctl enable ws-ovpn
 systemctl restart ws-ovpn
 
 # Getting Proxy Template
-wget -q -O /usr/local/bin/ws-tls https://${akbarvpn}/ws-tls
+wget -q -O /usr/local/bin/ws-tls https://raw.githubusercontent.com/anuy639/sc/main/websocket/ws-tls
 chmod +x /usr/local/bin/ws-tls
 
 # Installing Service
