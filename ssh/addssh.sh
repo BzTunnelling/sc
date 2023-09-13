d402a17af5bc6fdf5f4d2aa14bb523d5e13a4df4
BzTunnelling <144299592+BzTunnelling@users.noreply.github.com>
2023-09-13 20:50:50 +0800

    Update addssh.sh

--- ssh/addssh.sh
+++ ssh/addssh.sh
@@ -61,6 +61,73 @@ expi="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 hariini=`date -d "0 days" +"%Y-%m-%d"`
 expi=`date -d "$masaaktif days" +"%Y-%m-%d"`
+clear
+if [ ! -e /etc/ssh ]; then
+  mkdir -p /etc/ssh
+fi
+
+if [ -z ${Quota} ]; then
+  Quota="0"
+fi
+
+c=$(echo "${Quota}" | sed 's/[^0-9]*//g');
+d=$(( ${c} * 1024*1024*1024 ));
+
+if [[ ${c} != "0" ]]; then
+echo "${d}" > /etc/ssh/${user}
+fi
+DATADB=$(cat /etc/ssh/.ssh.db | grep "^###" | grep -w "${user}" | awk '{print $2}')
+if [[ "${DATADB}" != '' ]]; then
+  sed -i "/\b${user}\b/d" /etc/ssh/.ssh.db
+fi
+echo "### ${user} ${exp} ${uuid}" >>/etc/ssh/.ssh.db
+clear
+CHATID="$CHATID"
+KEY="$KEY"
+TIME="$TIME"
+URL="$URL"
+TEXT="<code>====================</code>
+<code>      XRAY/VMESS</code>
+<code>====================</code>
+<code>Remarks   : ${user}
+Domain         : ${domain}
+Username       : $Login 
+Password       : $Pass
+Dropbear       : 109, 143
+SSL/TLS        : $ssl
+Port Squid     : $sqd
+Ssh Ws SSL     : $ws
+Ssh Ws No SSL  : $ws2
+Ovpn Ws        : 2086
+Ovpn TCP       : $ovpn
+Ovpn UDP       : $ovpn2
+Ovpn SSL       : 990 
+BadVpn         : 7100-7200-7300
+Name      : SSH</code>
+<code>====================</code>
+<code>PAYLOAD WS TLS</code>
+<code>====================</code>
+<code>GET ws://${domain}/chat HTTP/1.1[crlf]Keep-Alive-Connection-Close[crlf]Upgrade: Websocket[crlf]Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==[crlf]Sec-WebSocket-Version: 13[crlf]Connection: Upgrade[crlf]User-Agent: [ua][crlf]Expect: 100-Continue[crlf]Content-Lenght: @anuybazoelk[crlf]Host: ${domain}[crlf]Connection: keep-alive[crlf]Proxy-Connection: keep-alive[crlf]X-Forwarded-For: ${domain}[crlf]X-Online-Host: ${domain}[crlf]X-Forward-Host: ${domain}[crlf][crlf]</code>
+<code>====================</code>
+<code>PAYLOAD WS NO TLS</code>
+<code>====================</code>
+<code>GET / HTTP/1.1[crlf]Host: ${domain}[crlf]Content-Lenght: @anuybazoelk639[crlf]Upgrade: Websocket[crlf][crlf]</code>
+<code>====================</code>
+<code>OVPN</code>
+<code>====================</code>
+OVPN TCP       : http://${domain}:89/tcp.ovpn
+OVPN UDP       : http://${domain}:89/udp.ovpn
+OVPN SSL       : http://${domain}:89/ssl.ovpn
+<code>====================</code>
+Dibuat Pada    : $hariini
+Berakhir Pada  : $expi
+Whatsapp       : https://wa.me/6287728411949
+Telegram       : https://t.me/anuybazoelk
+<code>====================</code>
+"
+
+curl -s --max-time $TIME -d "chat_id=$CHATID&disable_web_page_preview=1&text=$TEXT&parse_mode=html" $URL >/dev/null
+clear
 echo -e ""
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 echo -e "\E[44;1;39m      ⇱ SSH PREMIUM BZ OFFICIAL STORE ⇲          \E[0m"
